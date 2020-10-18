78bde6114aa1ea5c32e93e70622b024a3456ba52
Jason Manous <jcmanous@outlook.com>
2020-10-18 12:24:48 -0400

    created a project with cargo

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-579bcbd86b736808.d
@@ -0,0 +1,5 @@
+c:\Users\jcman\Workspace\repos\rust_sandbox\hello_cargo\target\rls\debug\deps\hello_cargo-579bcbd86b736808.rmeta: src\main.rs
+
+c:\Users\jcman\Workspace\repos\rust_sandbox\hello_cargo\target\rls\debug\deps\hello_cargo-579bcbd86b736808.d: src\main.rs
+
+src\main.rs:
